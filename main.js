ee759c40f5b4e2f51ca128b228475eba048c855e
John Keller <jmkeller3@gmail.com>
2018-05-21 11:58:20 -0500

    added autorization to get meetup api working

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+//Meetup API
+//autorization for OAuth
+const loginURL = `https://secure.meetup.com/oauth2/authorize?client_id=df773ak352f6trntvjgsvrg3n5&response_type=token&redirect_uri=https://guardiannw.github.io/test-meetup-api/index.html`;
+const url = window.location.href;
+const regex = /(?:#|\?|&)(?:([a-zA-Z_]+)=([^&]+))*/g;
+let matcher;
+let queryParams = {};
+while(matcher = regex.exec(url)) {
+    const [,key, val] = matcher;
+    queryParams[key] = val;
+}
+if (Object.keys(queryParams).length) {
+    // Authenticated
+    const requestURL = `https://api.meetup.com/2/concierge?access_token=${queryParams.access_token}&zip=38305&category_id=11&radius=smart`
+    console.log(queryParams);
+    $.ajax(requestURL, {
+        dataType: 'jsonp',
+        success: function (data) {
+            console.log(data);
+        }
+    });
+}
+else
+    window.location.href = loginURL;
+
 const MEETUP_SEARCH_URL = 'https://api.meetup.com/2/concierge?sign=true&photo-host=public&category_id=11&key=e5f40e367a304f44178202d444470';
 
 function getDatafromMeetUp(location, callback) {
@@ -5,8 +30,6 @@ function getDatafromMeetUp(location, callback) {
        category_id: 11,
        radius: 1,
        zip: location,
-    //    state: location,
-    //    city: location,
     }
     $.getJSON(MEETUP_SEARCH_URL, query, callback)
     console.log(`getData ran`)
@@ -50,32 +73,32 @@ function watchSubmit() {
 }
 
 $(watchSubmit);
+//google map api
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: new google.maps.LatLng(2.8,-187.3),
+    mapTypeId: 'terrain'
+  });
 
-// var map;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 2,
-//     center: new google.maps.LatLng(2.8,-187.3),
-//     mapTypeId: 'terrain'
-//   });
+  // Create a <script> tag and set the USGS URL as the source.
+  var script = document.createElement('script');
+  // This example uses a local copy of the GeoJSON stored at
+  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+  script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
 
-//   // Create a <script> tag and set the USGS URL as the source.
-//   var script = document.createElement('script');
-//   // This example uses a local copy of the GeoJSON stored at
-//   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-//   script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-//   document.getElementsByTagName('head')[0].appendChild(script);
-// }
-
-// // Loop through the results array and place a marker for each
-// // set of coordinates.
-// window.eqfeed_callback = function(results) {
-//   for (var i = 0; i < results.features.length; i++) {
-//     var coords = results.features[i].geometry.coordinates;
-//     var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//     var marker = new google.maps.Marker({
-//       position: latLng,
-//       map: map
-//     });
-//   }
-// }
+// Loop through the results array and place a marker for each
+// set of coordinates.
+window.eqfeed_callback = function(results) {
+  for (var i = 0; i < results.features.length; i++) {
+    var coords = results.features[i].geometry.coordinates;
+    var latLng = new google.maps.LatLng(coords[1],coords[0]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+}
