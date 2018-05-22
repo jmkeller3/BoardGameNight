519a8abc4858e332801f5a2fe96b404e88d6ec69
John Keller <jmkeller3@gmail.com>
2018-05-22 12:50:16 -0500

    more tweeks to app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const MeetUp_API_URL = 'https://api.meetup.com/2/concierge?zip=84043&offset=0&format=json&category_id=11&photo-host=public&page=500&sig_id=254699968&sig=9dfeb60b3e337a30d4407e49010a86088996c42a';
 
-function getData(url, callback)
-    $.getJSON(MeetUp_API_URL, callback)
+$.ajax(MeetUp_API_URL, {
+    dataType: 'jsonp',
+    success: function (data) {
+        console.log(data);
+        console.dir(data);
+    }
+})
 
 function renderMeetupResults(result) {
     console.log(`renderMeetup ran`);
