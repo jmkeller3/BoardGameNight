1e83b406b3264d6899a8e73061ebb3651f232ce7
John Keller <jmkeller3@gmail.com>
2018-05-26 09:37:49 -0500

    test2

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ if (Object.keys(queryParams).length) {
         user_lon = position.coords.longitude;
         console.log(`${user_lat} is ${position.coords.latitude} and is working`)
     });
+    console.log(`${user_lon} is ${position.coords.longitude} and is working`)
     const requestURL = `https://api.meetup.com/2/concierge?access_token=${queryParams.access_token}&lon=${user_lon}&category_id=11&radius=smart&lat=${user_lat}`
     console.log(queryParams);
     getLocation();
